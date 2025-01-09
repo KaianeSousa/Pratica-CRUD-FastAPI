6b65efa1684824bf6358a77d8b0751705df1b782
KaianeSousa <132933423+KaianeSousa@users.noreply.github.com>
2025-01-09 19:11:22 -0300

    Add files via upload

---
+++ Docker-db-Fastapi/app/schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class DoadorBase(BaseModel):
+    nome: str
+    idade: int
+    tipo_sanguineo: str
+    data_da_ultima_doacao: str
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        from_attributes = True
+
+class RecebedorBase(BaseModel):
+    nome: str
+    idade: int
+    tipo_sanguineo: str
+    necessidades_de_sangue: str
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        from_attributes = True
+
+class DoacaoBase(BaseModel):
+    doador_id: int
+    recebedor_id: int
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        from_attributes = True
